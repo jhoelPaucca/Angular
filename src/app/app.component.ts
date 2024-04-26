295dbe4a1b5d2aff2dd16b02939179b14e13689a
jhoelPaucca <pauccajhoel@gmail.com>
2024-04-26 10:34:30 -0500

    actualizacion del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-app';
+   title: String = 'Hola Kbrazo "danii" xd';
+    
 }
