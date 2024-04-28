420849b4d74a15f041f8be0d84a528e7579f3f8d
jhoelPaucca <pauccajhoel@gmail.com>
2024-04-28 16:51:10 -0500

    Mensaje de commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-   title: String = 'Hola Kbrazo "danii" xd';
-    
+
+  title: string = 'Hola angular mundo dede componente';
+  
+  users: string[] = [ 'Pepe', 'Maria', 'Juan', 'Andres'];
+  
+
+  visible: boolean = false;
+
+  setVsible(): void {
+    this.visible = this.visible? false:true;
+    console.log('hemos hecho clic en setVisible')
+  }
 }
